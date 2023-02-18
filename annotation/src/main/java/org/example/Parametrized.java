6eadc891196fba2ca9bf1829c9fce85b4a15f366
Серов Андрей <actsituation@gmail.com>
2023-02-19 01:52:34 +0700

    annotation

--- annotation/src/main/java/org/example/Parametrized.java
+++ annotation/src/main/java/org/example/Parametrized.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.math.BigDecimal;
+
 /**
  * @author andreiserov
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Parametrized {
 
     int[] numbers() default {1, 2, 3};
@@ -16,7 +21,33 @@ public @interface Parametrized {
 
     Class<? extends Number> number()
         default Integer.class;
+
+}
+@Parametrized(
+    enumVal = SomeEnum.ENUM,
+    annoParam = @Inner,
+    annoParams = {@Inner, @Inner},
+    number = BigDecimal.class
+)
+@interface Inner {}
+enum SomeEnum {
+    ENUM;
+
+    public static void main(String[] args) {
+
+        short val = 1234;
+
+        final Short aShort = new Short(val);
+
+
+        final Class<Inner> clazz = Inner.class;
+        if (clazz.isAnnotationPresent(Parametrized.class)) {
+            final Parametrized annotation = clazz.getAnnotation(Parametrized.class);
+            final String value = annotation.value();
+
+            System.out.println(value + " world!");
+        }
+    }
 }
 
-@interface Inner {}
-enum SomeEnum { }
+
